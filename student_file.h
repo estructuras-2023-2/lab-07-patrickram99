1fcb76d2fb781924536f68edd837ee30b823d97a
Patrick Ramirez <pramirezs@ulasalle.edu.pe>
2023-11-03 22:03:40 -0500

    Ahora pasan todos los test locales

--- student_file.h
+++ student_file.h
@@ -3,32 +3,68 @@
 
 using namespace std;
 
+struct Valor {
+  int value;
+  int count;
+
+  Valor(int x, int y) {
+    value = x;
+    count = y;
+  }
+};
+
 vector<int> Torneo(vector<int> input, int N, int K) {
   vector<int> output;
+  vector<Valor> valores;
 
-  while (K > 0) {
-    if (input[0] > input[1]) {
-      auto temp = input[1];
-      input[1] = input[0];
-      input[0] = temp;
+  for (int i : input) {
+    valores.push_back(Valor(i, 0));
+  }
+
+  while (K >= 1) {
+    if (valores[0].count == N) {
+      valores[0].count = 0;
+      valores.push_back(valores[0]);
+      valores.erase(valores.begin());
     }
 
+    if (valores[0].value > valores[1].value) {
+      valores[0].count += 1;
+      valores[1].count = 0;
+
+      auto temp = valores[1];
+      valores[1] = valores[0];
+      valores[0] = temp;
+    } else {
+      valores[1].count += 1;
+      valores[0].count = 0;
+    }
+
+    if (K == 1) {
+      break;
+    }
+
+    // cout << "jkdassds  " << valores[0].value << endl;
+    valores.push_back(valores[0]);
+    valores.erase(valores.begin());
+
+    /*
     cout << "Iter: " << K << endl;
-    for (auto i : input) {
-      cout << i << endl;
+    for(auto i: valores){
+      cout << i.value << " count: " << i.count << endl;
     }
-
-    input.push_back(input[0]);
-    input.erase(input.begin());
+    */
 
     K -= 1;
   }
 
-  output.push_back(input[0]);
-  output.push_back(input[1]);
+  output.push_back(valores[0].value);
+  output.push_back(valores[1].value);
+
   cout << "Final: " << K << endl;
   for (auto i : output) {
     cout << i << endl;
   }
+
   return output;
 }
